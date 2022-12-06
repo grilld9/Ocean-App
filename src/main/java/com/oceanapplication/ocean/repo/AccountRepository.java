12c15e93f1abc7187a26711bbd9fa8b44c015dd5
grilld9 <m.maksimenkov@g.nsu.ru>
2022-12-06 18:00:25 +0700

    Добавил метод для проверки регистрации

--- src/main/java/com/oceanapplication/ocean/repo/AccountRepository.java
+++ src/main/java/com/oceanapplication/ocean/repo/AccountRepository.java
@@ -2,7 +2,8 @@ package com.oceanapplication.ocean.repo;
 
 import com.oceanapplication.ocean.models.Account;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
-
+    Optional<Account>  findByPhoneNumber(String phoneNumber);
 }
