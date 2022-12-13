287884bae5d5e4f55a149368534ede12e60792fb
mkrikunov <m.krikunov@g.nsu.ru>
2022-12-14 02:19:46 +0700

    добавил Library.java с черновиком пост запроса для регистрации

--- app/src/main/java/com/app/ocean/RegistrationActivity.java
+++ app/src/main/java/com/app/ocean/RegistrationActivity.java
@@ -1,32 +1,33 @@
 package com.app.ocean;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.io.IOException;
 
 import okhttp3.Call;
-import okhttp3.FormBody;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText name, phoneNumber, password;
     Button regButton;
     boolean passwordVisible;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +39,8 @@ public class RegistrationActivity extends AppCompatActivity {
         password = findViewById(R.id.inputPasswordR);
 
         regButton = findViewById(R.id.buttonReg);
+
+
 
         //Operation of the icon hiding the password:
         password.setOnTouchListener(new View.OnTouchListener() {
@@ -74,41 +77,33 @@ public class RegistrationActivity extends AppCompatActivity {
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //send post request
                 String nameVal = name.getText().toString();
                 String phoneNumberVal = phoneNumber.getText().toString();
-                //String passwordVal = password.getText().toString();
+                String passwordVal = password.getText().toString();
 
+                String url = "http://localhost:8080/demo/add?name=" + nameVal +
+                        "&phoneNumber=" + phoneNumberVal + "&password=" + passwordVal;
 
-                OkHttpClient client = new OkHttpClient();
+                
+                //Library.main(nameVal, phoneNumberVal, passwordVal);
 
-                RequestBody requestBody = new FormBody.Builder()
-                        .add("name", nameVal)
-                        .add("number", phoneNumberVal)
-                        .build();
-
-                Request postRequest = new Request.Builder()
-                        .url("jdbc:mysql://${MYSQL_HOST:localhost}:3306/ocean/account")
-                        .post(requestBody)
-                        .build();
-
-                try {
-                    Response response = client.newCall(postRequest).execute();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+                //move to main page
                 Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        });
 
+
+        });
     }
+
+
 
     //move to login page:
     public void moveToLogin(View v) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
 
 }
