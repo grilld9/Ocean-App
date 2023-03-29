fd03113b8b0e856f69a432cd38ee7476da73b552
Maxim <112863178+grilld9@users.noreply.github.com>
2023-03-29 09:46:12 +0700

    RegistrationService hotfix

--- src/main/java/com/oceanapplication/ocean/services/RegistrationService.java
+++ src/main/java/com/oceanapplication/ocean/services/RegistrationService.java
@@ -24,6 +24,9 @@ public class RegistrationService {
     private final RoleRepository roleRepository;
 
     public AuthResponseDTO register(RegistrationRequestDTO request) {
+        userRepository.findByPhoneNumber(request.getPhoneNumber())
+            .orElseThrow(() -> new UnsupportedOperationException(
+                "User " + request.getPhoneNumber() + " has already registered"));
         Set<Role> roles = new HashSet<>();
         roles.add(roleRepository.findByName("ROLE_USER")); //TODO test it
         var user = User.builder()
